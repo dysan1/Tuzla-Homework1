2fdca0da3823e6c70883cd1fc27ddcf667f87414
SandySmajic <sysc1223@aubih.edu.ba>
2014-06-10 18:54:34 +0200

    Homework
    Tuzla Homework

---
+++ Tuzla Homework1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tuzla_Homework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countNumbers;
+            int countErrors;
+
+            while(countErrors < 100)
+            {
+                if (countErrors == 5) break;
+            }
+            const string QuitString = "QUIT";
+            Console.WriteLine("Please input a number");
+
+            string input = Console.ReadLine();
+            double number = string
+                Console.WriteLine("Number is:" + );
+        }
+    }
+}
